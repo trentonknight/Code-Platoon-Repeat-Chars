c0b79ce5022164b794281bb0b41b2d551fabdcc8
trentonknight <trentonknight@protonmail.com>
2022-07-16 20:19:16 -0400

    modified:   index.js 	modified:   index.test.js

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-function repeatedChars(charArray, numArray) {
-	const iterator = numArray.keys();
-	const wordup = [];
+function repeatedChars (charArray, numArray) {
+  const iterator = numArray.keys()
+  const wordup = []
 
-	for (const key of iterator) {
-		for (incr = 0; incr < numArray[key]; incr++){
-			wordup.push(charArray[key]);
-	}
-
+  for (const key of iterator) {
+    for (let incr = 0; incr < numArray[key]; incr++) {
+      wordup.push(charArray[key])
+    }
+  }
+  return wordup.join('')
 }
-return wordup.join('');
-}
 
-module.exports = repeatedChars;
+module.exports = repeatedChars
 
-console.log(repeatedChars(['a','b','c'], [1,2,3])); 
-console.log(repeatedChars(['h','e','l','o'], [1,1,2,1])); 
-
+console.log(repeatedChars(['a', 'b', 'c'], [1, 2, 3]))
+console.log(repeatedChars(['h', 'e', 'l', 'o'], [1, 1, 2, 1]))
--- index.test.js
+++ index.test.js
@@ -1,5 +1,6 @@
-const repeatedChars = require('./index');
+const repeatedChars = require('./index')
 
 test('Converts two arrays to single String', () => {
-	expect(repeatedChars(['a','b','c'], [1,2,3])).toContain('abbccc');
-});
+  expect(repeatedChars(['a', 'b', 'c'], [1, 2, 3])).toContain('abbccc')
+  expect(repeatedChars(['h', 'e', 'l', 'o'], [1, 1, 2, 1])).toContain('hello')
+})
